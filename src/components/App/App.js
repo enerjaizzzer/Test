52a7645bcd67f189626f4c1d9227af42846f8018
enerjaizzzer <enerjaizzzer@gmail.com>
2019-08-29 10:22:04 +0300

    init: test project

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
